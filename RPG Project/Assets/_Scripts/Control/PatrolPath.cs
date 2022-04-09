bf875865a28bb670fd212fd30209c62be2b8b72d
Rea <lathindu2000@gmail.com>
2022-04-10 01:01:57 +0530

    Waypoint Visualization With Gizmos

---
+++ RPG Project/Assets/_Scripts/Control/PatrolPath.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace RPG.Control
+{
+    public class PatrolPath : MonoBehaviour
+    {
+
+        const float wayPointGizmoRadius = 0.2f;
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = Color.white;
+            for (int i = 0; i < transform.childCount; i++)
+            {
+                int j = GetNextIndex(i);
+                Gizmos.DrawSphere(GetWaypoint(i), wayPointGizmoRadius);
+                Gizmos.DrawLine(GetWaypoint(i), GetWaypoint(j));
+
+
+            }
+        }
+
+        private  int GetNextIndex(int i)
+        {
+            if(i + 1  == transform.childCount)
+            {
+                return 0;
+            }
+            return i + 1;
+        }
+
+        private Vector3 GetWaypoint(int i)
+        {
+            return transform.GetChild(i).position;
+        }
+    }
+}
