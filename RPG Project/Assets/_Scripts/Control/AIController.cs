b5db7084f58e8b97cc91ccedb130920f0bd5d397
Rea <lathindu2000@gmail.com>
2022-04-12 12:07:43 +0530

    Implemented the designed patrol path behaviour

--- RPG Project/Assets/_Scripts/Control/AIController.cs
+++ RPG Project/Assets/_Scripts/Control/AIController.cs
@@ -1,9 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 using RPG.Combat;
 using RPG.Core;
 using RPG.Movement;
+using System;
 
 namespace RPG.Control 
 
@@ -11,24 +13,42 @@ namespace RPG.Control
     public class AIController : MonoBehaviour
     {
 
+        //Object References 
         GameObject player;
         Fighter fighter;
         Health health;
+        Mover mover;
+       [SerializeField] PatrolPath patrolPath;
+        NavMeshAgent navMeshAgent;
+        
+
+
+        //Complex Vars
         Vector3 guardPosition;
+
+
+        //Primitives 
         [SerializeField] float suspicionTime = 5f;
-       
-        [SerializeField] float chaseDistance = 5f; 
+        [SerializeField] float chaseDistance = 5f;
+        [SerializeField] float waypointDwellTime = 3f;
         float timeSinceLastSawPlayer = Mathf.Infinity;
+        [SerializeField]float timeSinceArrivedAtWaypoint = Mathf.Infinity;
+        [SerializeField] float waypointTolerance = 1f;
+        private int currentWaypointIndex =  0;
+       
+        
+        
+
+     
+
         private void Start()
         {
             player = GameObject.FindWithTag("Player");
             fighter = GetComponent<Fighter>();
             health = GetComponent<Health>();
-
+            mover = GetComponent<Mover>();
+            navMeshAgent = GetComponent<NavMeshAgent>();
             guardPosition = transform.position;
-
-           
-
         }
 
         public void Update()
@@ -38,36 +58,94 @@ namespace RPG.Control
 
             if (InAttackRangeOfPlayer() && fighter.CanAttack(player))
             {
-                timeSinceLastSawPlayer = 0;
+                
 
                 AttackBehaviour();
             }
 
-            else if (timeSinceLastSawPlayer<suspicionTime)
+            else if (timeSinceLastSawPlayer < suspicionTime)
             {
                 SuspicionBehaviour();
             }
 
             else
             {
-                GuardBehaviour();
+                PatrolBehaviour();
             }
+
+            //becuase we are setting the timers to 0 at one point in code it doesnt really matter if we increment these time variables
+            UpdateTimers();
+
+        }
+
+
+
+
+
+
+
+
+
+
+        private void UpdateTimers()
+        {
             timeSinceLastSawPlayer += Time.deltaTime;
+            timeSinceArrivedAtWaypoint += Time.deltaTime;
+        }
+
+        private void PatrolBehaviour()
+        {
+            navMeshAgent.speed = 2;
+            Vector3 nextPosition = guardPosition;
+
+            if(patrolPath != null)
+            {
+                if (AtWaypoint() ) 
+                {
+                    timeSinceArrivedAtWaypoint = 0;
+                    CycleWaypoint();
+                }
+
+                nextPosition = GetCurrentWaypoint();
+            }
+
+            if(timeSinceArrivedAtWaypoint > waypointDwellTime)
+            {
+                mover.StartMoveAction(nextPosition);
+            }
             
         }
 
-        private void GuardBehaviour()
+
+        private bool AtWaypoint()
         {
-            GetComponent<Mover>().StartMoveAction(guardPosition);
+            
+            float distanceToWaypoint = Vector3.Distance(transform.position, GetCurrentWaypoint());
+            return distanceToWaypoint < waypointTolerance;
+
+        }
+        private Vector3 GetCurrentWaypoint()
+        {
+            return patrolPath.GetWaypoint(currentWaypointIndex);
+        }
+
+        private void CycleWaypoint()
+        {
+       
+            currentWaypointIndex = patrolPath.GetNextIndex(currentWaypointIndex);
+
         }
 
         private void SuspicionBehaviour()
         {
             GetComponent<ActionScheduler>().CancelCurrentAction();
+            
         }
 
         private void AttackBehaviour()
         {
+            timeSinceLastSawPlayer = 0;
+            navMeshAgent.speed = 3;
             fighter.Attack(player);
         }
 
